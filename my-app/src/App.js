3c5e151b815584d751f0b41bf696dcd8f2130dd3
danitiemi <danitiemi.br@gmail.com>
2018-10-03 12:39:31 -0700

    Header component and style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import me from './images/me.jpg';
-import './styles/App.scss';
+import React, { Component } from 'react'
+import './styles/App.scss'
 import NavBar from './components/NavBar'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
@@ -9,20 +9,8 @@ class App extends Component {
       <div className="App">
         <div className="background">
           <NavBar />
-          <header className="App-header">
-            <img src={me} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+          <Header />
+  
         </div>
       </div>
     );
--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -4,11 +4,11 @@ class NavBar extends Component {
   
   render() {
     return (
-      <div class="main-nav">    
-        <ul class="nav">
-            <li><i class="fas fa-laptop-code"></i><a href="https://github.com/danitiemi" target="_blank" rel="noopener noreferrer"> Projects</a></li>
-            <li><i class="far fa-file-code"></i><a href="https://resume.creddle.io/resume/irnesg6czb4" target="_blank"rel="noopener noreferrer"> Resume</a></li>
-            <li><i class="far fa-id-card"></i><a href="mailto:danitiemi.br@gmail.com"> Contact</a></li>
+      <div className="main-nav">    
+        <ul className="nav">
+            <li><i className="fas fa-laptop-code"></i><a href="https://github.com/danitiemi" target="_blank" rel="noopener noreferrer"> Projects</a></li>
+            <li><i className="far fa-file-code"></i><a href="https://resume.creddle.io/resume/irnesg6czb4" target="_blank"rel="noopener noreferrer"> Resume</a></li>
+            <li><i className="far fa-id-card"></i><a href="mailto:danitiemi.br@gmail.com"> Contact</a></li>
         </ul>     
       </div>
     )
@@ -16,4 +16,4 @@ class NavBar extends Component {
 
 }
 
-export default NavBar;
+export default NavBar
