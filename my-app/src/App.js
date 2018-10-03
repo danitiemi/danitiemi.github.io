476ea6dade5e9874ac1505108302815fbaa930d2
danitiemi <danitiemi.br@gmail.com>
2018-10-03 13:00:59 -0700

    Footer component and styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './styles/App.scss'
 import NavBar from './components/NavBar'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
@@ -10,7 +11,7 @@ class App extends Component {
         <div className="background">
           <NavBar />
           <Header />
-  
+          <Footer />
         </div>
       </div>
     );
